10947511a11b8727c61036d5fc8318dfb65edce2
Nicolas Acuna <nicoacu87@gmail.com>
2021-11-17 08:27:34 -0300

    Updated fourth exercise.

--- nicoacu/04. Fourth Exercise/src/Models/Interfaces/TransferMarket.java
+++ nicoacu/04. Fourth Exercise/src/Models/Interfaces/TransferMarket.java
@@ -4,11 +4,7 @@ import Models.Exceptions.InvalidBudgetException;
 
 public interface TransferMarket {
 
-    //Applying new method for Polymorphism.
-    //Polymorphism applied on Inter and RealMadrid teams, as these teams have another parameter in the method called "canAskForLoan"
-    //BocaJuniors teams is using the default method as an example.
-
-    default void canBuyPlayers() throws InvalidBudgetException {
+    default void canBuyPlayers(int playerCost) throws InvalidBudgetException {
 
     }
 
